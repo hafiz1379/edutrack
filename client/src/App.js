9e00af0bf68044ec32d819ace640e568ac72cc30
hafiz1379 <hafizrasa1379@gmail.com>
2025-08-15 14:31:46 +0430

    add FeePaymentsPage and SalaryPaymentsPage routes to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import ClassesPage from "./pages/ClassesPage";
+import StudentsPage from "./pages/StudentsPage";
+import TeachersPage from "./pages/TeachersPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+// Add this import
+import FeePaymentsPage from "./pages/FeePaymentsPage";
+import SalaryPaymentsPage from "./pages/SalaryPaymentsPage";
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold text-center mt-4 text-blue-500'>Hello World</h1>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="classes" element={<ClassesPage />} />
+            <Route path="students" element={<StudentsPage />} />
+            <Route path="teachers" element={<TeachersPage />} />
+            <Route path="fees" element={<FeePaymentsPage />} />
+            <Route path="salaries" element={<SalaryPaymentsPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
